b31c8e069ffde8bb29d8f37d8326f4db459c3e55
ayokunle adeleye <adeways2000@gmail.com>
2021-05-13 03:15:20 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(numberPalindrome.isPalindrome(-1221));
+        System.out.println(numberPalindrome.isPalindrome(707));
+        System.out.println(numberPalindrome.isPalindrome(11212));
+    }
+}
